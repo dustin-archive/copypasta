242e19c789fec5bde6678306e5ad1234457687d1
Dustin Dowell <dustindowell22@gmail.com>
2017-04-07 19:15:58 -0500

    So it begins...

---
+++ index.js
@@ -0,0 +1,23 @@
+const prompter = require('cli-prompter')
+const template = require('fs-template')
+
+const questions = [{
+  type: 'text',
+  name: 'name',
+  message: 'Give your app a name',
+  default: 'john'
+}]
+
+prompter(questions, (err, values) => {
+  if (err) throw err
+
+  const options = {
+    source: '',
+    target: '',
+    vars: values
+  }
+
+  template(options, (err) => {
+    if (err) throw err
+  })
+})
